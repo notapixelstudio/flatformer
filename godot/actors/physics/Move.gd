7396d03038fb97f6a49b052c0986ab3f9e5580ab
Henrique Campos <henriquecampos@pigdev.studio>
2019-08-03 16:08:45 -0300

    Add support for acceleration and deceleration curves
    Use the inspector to draw how the character should accelerate and decelerate, there is a nice EASE curve, the higher the value the higher the acceleration/friction

--- godot/actors/physics/Move.gd
+++ godot/actors/physics/Move.gd
@@ -2,11 +2,20 @@ extends "res://actors/physics/PlatformCommand.gd"
 
 export (float) var speed = 400.0
 export (Vector2) var movement_direction = Vector2.ZERO setget set_direction
+export (float, EASE) var acceleration_factor = 1.0
+
+
+func _physics_process(delta):
+	if abs(actor.velocity.x) < speed:
+		actor.velocity.x = lerp(actor.velocity.x, movement_direction.x * speed, acceleration_factor * delta)
+	else:
+		set_physics_process(false)
+
 
 func execute():
 	if not enabled:
 		return
-	actor.velocity.x = movement_direction.x * speed
+	set_physics_process(true)
 	emit_signal("started")
 
 
@@ -15,4 +24,5 @@ func set_direction(new_direction):
 
 
 func cancel():
+	set_physics_process(false)
 	emit_signal("finished")
