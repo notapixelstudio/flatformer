b4c79626caa0da2ecf0a35e2de5178a2a48e1aa5
salvob <tholalak@yahoo.it>
2019-08-03 18:18:06 +0200

    clean up from print

--- godot/actors/1DController.gd
+++ godot/actors/1DController.gd
@@ -1,10 +1,10 @@
 extends PathFollow2D
 
+
 var velocity = Vector2.ZERO setget set_velocity
 
 func _physics_process(delta):
 	offset += velocity.x * delta
-	print(offset)
 
 
 func set_velocity(new_velocity):
