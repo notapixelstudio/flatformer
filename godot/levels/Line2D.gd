70474e5dc3855fdbeb7191a629c4a5684285c46c
salvob <tholalak@yahoo.it>
2019-08-04 01:35:30 +0200

    cleanup, added collision and restart

--- godot/levels/Line2D.gd
+++ godot/levels/Line2D.gd
@@ -4,5 +4,10 @@ onready var path = $".."
 
 
 func _ready():
+	var i = 0
 	for point in path.curve.get_baked_points():
 		add_point(point)
+		if i %10 == 0:
+			yield(get_tree().create_timer(0.01), "timeout")
+		i +=1
+		
